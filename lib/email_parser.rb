d429bbf50aa6bec7862fd69cf3b67304a35a55d6
jessi ortega <ortegabx05@gmail.com>
2019-08-01 20:05:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,11 +11,4 @@ class EmailParser
     @emails = emails
   end
 
-  def parse
-    email_array = []
-    email_array = @emails.split(/[,\s]/)
-    email_array = email_array.reject { |e| e == ""}
-    email_array.uniq
-  end
-
 end
