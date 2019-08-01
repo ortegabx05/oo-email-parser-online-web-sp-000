cf5ed00b9023c31d5b94a2b6a4a312c79121837a
jessi ortega <ortegabx05@gmail.com>
2019-08-01 20:04:48 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,15 +4,18 @@
 # or whitespace (' ').
 
 class EmailParser
-	attr_accessor :emails
-	
-	def initialize(emails)
-		@emails= emails
-	end
-	
-	def parse
-		@emails = @emails.gsub(/[\s,]/ ," ").split
-		@emails.uniq
-	end
-	
+
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    email_array = []
+    email_array = @emails.split(/[,\s]/)
+    email_array = email_array.reject { |e| e == ""}
+    email_array.uniq
+  end
+
 end
